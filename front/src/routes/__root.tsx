f91e20b8e10889a68a3346f9d489cd12b754d27e
ismaiilov-atai <ataismaiilov@gmail.com>
2025-06-20 23:58:02 -0400

    page: not found page added

--- front/src/routes/__root.tsx
+++ front/src/routes/__root.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Outlet, createRootRoute, useLocation } from '@tanstack/react-router';
+import { NotFound } from '@/components/NotFound';
 import Header from '../components/Header';
 import { Toaster } from 'sonner';
 
@@ -7,6 +8,7 @@ const queryClient = new QueryClient();
 
 export const Route = createRootRoute({
   component: Root,
+  notFoundComponent: NotFound,
 });
 
 function Root() {
